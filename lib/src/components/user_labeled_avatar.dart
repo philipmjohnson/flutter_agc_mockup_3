13e319c81542dd18d391a4b14aca514dc0de32dd
Philip Johnson <johnson@hawaii.edu>
2022-12-21 09:00:34 -1000

    initial commit

---
+++ lib/src/components/user_labeled_avatar.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_agc_mockup/src/components/user_avatar.dart';
+
+/// Provides a CircleAvatar with either an image if available or initials, plus a label.
+class UserLabeledAvatar extends StatelessWidget {
+  const UserLabeledAvatar(
+      {Key? key,
+      required this.userID,
+      this.label = '',
+      this.rightPadding = 0})
+      : super(key: key);
+
+  final String userID;
+  final String label;
+  final double rightPadding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(children: [UserAvatar(userID: userID), Text(label)]),
+        SizedBox(width: rightPadding)
+      ],
+    );
+  }
+}
